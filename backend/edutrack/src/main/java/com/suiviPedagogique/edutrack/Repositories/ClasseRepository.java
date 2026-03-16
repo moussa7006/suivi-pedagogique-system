7dad4cb374d54361e75063dae8c04bdaea29da09
moussa7006 <moussa.b.keita223@gmail.com>
2026-03-16 17:15:47 +0000

    feat: mise en place des repositories

---
+++ backend/edutrack/src/main/java/com/suiviPedagogique/edutrack/Repositories/ClasseRepository.java
@@ -0,0 +1,9 @@
+package com.suiviPedagogique.edutrack.Repositories;
+
+import com.suiviPedagogique.edutrack.Entities.Classe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClasseRepository extends JpaRepository<Classe, Integer> {
+}
---
+++ backend/edutrack/src/main/java/com/suiviPedagogique/edutrack/Repositories/EnseignantRepository.java
@@ -0,0 +1,10 @@
+package com.suiviPedagogique.edutrack.Repositories;
+
+import com.suiviPedagogique.edutrack.Entities.Enseignant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnseignantRepository extends JpaRepository<Enseignant, Integer> {
+
+}
